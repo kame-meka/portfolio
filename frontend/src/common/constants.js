7e16765ebb8c15031d1dde4dce3b4aeb8ecbce72
kame-meka <tsurumi.in.dus.tri.al@gmail.com>
2024-06-24 17:13:24 +0900

    add front header

---
+++ frontend/src/common/constants.js
@@ -0,0 +1,28 @@
+export default {
+  menuItems: [
+    {
+      name: 'HOME',
+      url: '#'
+    },
+    {
+      name: 'ABOUT',
+      url: '#'
+    },
+    {
+      name: 'WORK',
+      url: '#'
+    },
+    {
+      name: 'SERVICE',
+      url: 'https://www.notion.so/d43109f934104fa08706a149e11d9ffc'
+    },
+    {
+      name: 'BLOG',
+      url: '#'
+    },
+    {
+      name: 'ACCESS',
+      url: '#'
+    }
+  ]
+}
